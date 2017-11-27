98723dd0b941d0ffff47b4701848386cd02e9fb4
Mario Andres Ravina <ayarmarxio@gmail.com>
2017-11-27 14:33:10 +0100

    AttendantsListPage Xaml Cs Disable title bar

--- Fitspiraton/View/AttendantsListPage.xaml.cs
+++ Fitspiraton/View/AttendantsListPage.xaml.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.Core;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -25,6 +28,12 @@ namespace Fitspiraton.View
         public AttendantsListPage()
         {
             this.InitializeComponent();
+
+            // To disable Title Bar
+            ApplicationViewTitleBar formattableTitleBar = ApplicationView.GetForCurrentView().TitleBar;
+            formattableTitleBar.ButtonBackgroundColor = Colors.Transparent;
+            CoreApplicationViewTitleBar coreTitleBar = CoreApplication.GetCurrentView().TitleBar;
+            coreTitleBar.ExtendViewIntoTitleBar = true;
         }
     }
 }
