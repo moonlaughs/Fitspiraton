10a9b9cc69b1aaa15d7ca086fc64c250b986cd33
Daniel Eckmann <dani337x@edu.easj.dk>
2017-12-05 17:25:42 +0100

    Added ActivityPage, binded the userwith it. Added some pictures.

--- Fitspiraton/View/UserMenu.xaml.cs
+++ Fitspiraton/View/UserMenu.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Fitspiraton.ViewModel;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -40,6 +41,12 @@ namespace Fitspiraton.View
         private void BackBtn_OnClicked(object sender, RoutedEventArgs e)
         {
             Frame.Navigate(typeof(MainPage));
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            LoginVm log = new LoginVm();
+            Frame.Navigate(typeof(ActivitySelectionPage),log.CurrentUser);
         }
     }
 }
