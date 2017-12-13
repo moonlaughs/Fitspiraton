b93bf34d40596c85305f49b8cae810675a202f7d
Mario Andres Ravina <ayarmarxio@gmail.com>
2017-12-13 15:30:05 +0100

    TemporaryChanges

--- Fitspiraton/View/ProfileView.xaml.cs
+++ Fitspiraton/View/ProfileView.xaml.cs
@@ -12,6 +12,8 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Microsoft.Toolkit.Uwp;
+
 
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
